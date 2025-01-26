b5d39ea26cc68e8ebecddbd42612df3103275b65
OriginalPid <bondarenkoandriy98@gmail.com>
2025-01-26 13:31:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from random import randint
 
 init()
 W, H = 600, 700
+FPS = 60
+
+mixer.init()
+mixer.music.load('sounds/space.ogg')
+mixer.music.set_volume(0.6)
+mixer.music.play()
+
+fire_snd = mixer.Sound('sounds/fire.ogg')
+
+font.init()
+font1 = font.SysFont('fonts/Bebas_Neue_Cyrillic.ttf', 35, bold=True)
+font2 = font.SysFont('fonts/Bebas_Neue_Cyrillic.ttf', 100, bold=True)
+
 
 window = display.set_mode((W, H))
 display.set_caption('Space Shooter')
@@ -33,44 +46,147 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys_pressed[K_d] and self.rect.x < W - self.width:
             self.rect.x += self.speed
-
-player = Player(W/2, H - 100, 80, 100, 15, 'images/rocket.png')
-
-
-life = 3
-killed = 0 
-skipped = 0
-
+    def fire(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top, 16, 20, 20, 'images/bullet.png')
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
-        global killed 
+        global skipped 
         self.rect.y += self.speed
         if self.rect.y > H - self.height:
             self.rect.x = randint(0, H - self.width)
             self.rect.y = 0
-            killed += 1
+            skipped += 1
 
+
+class Asteroid(GameSprite):
+    def __init__(self, x, y, width, height, speed, img):
+        super().__init__(x, y, width, height, speed, img)
+        self.angle = 0
+        self.original_image = self.image
+
+    def update(self):
+        self.rect.y += self.speed
+        self.angle = (self.angle + 2.5) % 360
+        self.image = transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        if self.rect.y > H - self.height:
+            self.rect.x = randint(0, W - self.width)
+            self.rect.y = 0
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+class Buff(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y > H - self.height:
+            self.rect.x = randint(0, W - self.width)
+            self.rect.y = -100
+
+player = Player(W/2, H - 100, 80, 100, 10, 'images/rocket.png')
+life_boost = Buff(randint(0, W - 50), -100, 50, 50, randint(2, 5),'images/Aid.png')
+bullets_boost = Buff(randint(0, W - 50), -100, 50, 50, randint(2, 5),'images/Ammo.jpeg')
 enemies = sprite.Group()
 for i in range(5):
-    enemy = Enemy(randint(0, H), 0, 70, 35, 2, 'images/ufo.png' )
+    enemy = Enemy(randint(0, W - 70), randint(-35, 10), 70, 35, randint(1, 3), 'images/ufo.png')
     enemies.add(enemy)
 
+asteroids = sprite.Group()
+for i in range(3):
+    asteroid = Asteroid(randint(0, W - 70), randint(-35, 10), 70, 35, randint(1, 3), 'images/asteroid.png')
+    asteroids.add(asteroid)
 
+bullets = sprite.Group()
+
+life = 3
+killed = 0 
+skipped = 0
+shoot_count = 30
+game = True
+finish = False
 
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                if shoot_count > 0:
+                    fire_snd.play()
+                    player.fire()
+                    shoot_count -= 1
+                    if e.key == K_ESCAPE:
+                        finish = False
+    if not finish:
 
-    window.blit(bg, (0, 0))
-    player.draw()
-    player.move()
-    enemies.draw(window)
-    enemies.update()
+
+        window.blit(bg, (0, 0))
+        player.draw()
+        player.move()
+
+        enemies.draw(window)
+        enemies.update()
+        bullets.draw(window)
+        bullets.update()
+        life_boost.draw()
+        life_boost.update()
+        bullets_boost.draw()
+        bullets_boost.update()
+        if sprite.groupcollide(bullets, enemies, True, True):
+            killed += 1
+            enemy = Enemy(randint(0, W - 70), randint(-35, 10), 70, 35, randint(1, 3), 'images/ufo.png')
+            enemies.add(enemy)
+        
+        if sprite.groupcollide(bullets, enemies, True, False):
+            pass
+
+        if sprite.spritecollide(player, asteroids, True):
+            life -= 1
+            asteroid = Asteroid(randint(0, W - 70), randint(-35, 10), 70, 35, randint(1, 3), 'images/asteroid.png')
+            asteroids.add(asteroid)
+
+        if sprite.spritecollide(player, enemies, True):
+            life -= 1
+            enemy = Enemy(randint(0, W - 70), randint(-35, 10), 70, 35, randint(1, 3), 'images/ufo.png') 
+            enemies.add(enemy)
+
+        if life < 0:
+            finish = True
+            mixer.music.stop()
+
+        asteroids.draw(window)
+        asteroids.update()
+
+        skipped_txt = font1.render(f'Пропущено: {skipped}', True, (255, 255, 255))
+        window.blit(skipped_txt, (10, 10))
+        killed_txt = font1.render(f'Збито:{killed}', True, (255, 255, 255))
+        life_txt = font2.render(str(life), True, (0, 255, 0))
+        bullets_txt = font1.render(f'Кулі: {shoot_count}', True, (255, 255, 255))
+
+
+        window.blit(skipped_txt, (10, 10))
+        window.blit(killed_txt, (10, 40))
+        window.blit(life_txt, ( W - 50, 40))
+        window.blit(bullets_txt, (10, 80))
+    else:
+        life = 3
+        skipped = 0
+        killed = 0
+        for enemy in enemies:
+            enemy.kill()
+        for asteroid in asteroids:
+            asteroid.kill()
+        for bullet in bullets:
+            bullets.kill()
+
     display.update()
-    clock.tick(60)
+    clock.tick(FPS)
 
 
 
